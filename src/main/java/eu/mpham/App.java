f201f721629804e70a2745a04349be234ee829dc
Mickael Pham <mickael.pham@zuora.com>
2012-08-16 09:35:12 -0700

    Added logback.xml file

--- src/main/java/eu/mpham/App.java
+++ src/main/java/eu/mpham/App.java
@@ -7,15 +7,17 @@ import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.util.StatusPrinter;
 
 public class App {
+	
+	final static Logger logger = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) {
-
-		Logger logger = LoggerFactory.getLogger(App.class);
-		logger.debug("Hello World!");
-
-		// print internal state
 		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
 		StatusPrinter.print(lc);
+		
+		logger.info("Entering application.");
+		Foo foo = new Foo();
+		foo.doIt();
+		logger.info("Existing application.");
 	}
 
 }
